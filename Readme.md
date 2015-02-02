10d594a3af7fad964adf9bc148d74fcff572ba96
Katherine Trammell <ktrammell@gmail.com>
2015-02-02 13:52:24 -0500

    Added the answers to the homework questions and updated the ReadMe file

---
+++ Readme.md
@@ -0,0 +1,3 @@
+The purpose of the normal.js file is to run through set questions and determine the responses that the Javascript would return.
+
+There are two querstions in which the user must write the Javascript lines that would give a particular output.
--- normal.js
+++ normal.js
@@ -1,49 +1,49 @@
 var name;
 
 // Q: What is the value of name?
-
+// ""
 
 
 name = "JD";
 
 // Q: What is the value of name?
-
+// JD
 
 
 name = "Toni"
 
 // Q: What is the value of name?
-
+// Toni
 
 
 var a = 5;
 
 // Q: What is the value of a?
-
+// 5
 
 
 var b = 10;
 
 // Q: What is the value of b?
-
+// 10
 
 
 a = b;
 
 // Q: What is the value of a?
-
+// 10
 
 
 b = 20;
 
 // Q: What is the value of a?
-
+// 20
 
 
 var greeting = "Hello " + name + ", how are you?";
 
 // Q: What is the value of greeting?
-
+// HelloToni, how are you?
 
 
 var high = 50;
@@ -52,14 +52,14 @@ var low = 10;
 var delta = high - low;
 
 // Q: What is the value of delta?
-
+// 40
 
 
 
 var math = delta + 20 - high;
 
 // Q: What is the value of math?
-
+// 10
 
 
 
@@ -69,6 +69,7 @@ var surName = "Isaacks";
 // Using the variables above create another variable
 // with the content "My Instructor's name is JD Isaacks"
 
+// var content = "My Instructor's name is" + " " + givenName + " " + surName;
 
 
 
@@ -78,4 +79,5 @@ var lastBirthday = 2014;
 // Using the varibles above create a new variable that
 // holds the value that is my age (30)
 
+// var age = lastBirthday - birthYear;
 
